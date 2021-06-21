d2518ff3127b645eb16ff698488951dffb3e8b28
Jeremy Ivan <59781543+JIARC777@users.noreply.github.com>
2021-06-21 12:01:35 -0700

    Removed Commented Code

--- GOB/Assets/Action.cs
+++ GOB/Assets/Action.cs
@@ -2,39 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/*
-public class Action
-{
-    public string actionName;
-    public string effectingGoal;
-    public float goalEffect;
-    public float duration;
-    
-    public Action(string actionName,string effectingGoal, float goalEffect, float duration)
-    {
-        this.actionName = actionName;
-        this.effectingGoal = effectingGoal;
-        this.goalEffect = goalEffect;
-        this.duration = duration;
-    }
-    public virtual float GetGoalChange(Goal goal) {
-        return ((effectingGoal == goal.goalName) ? goalEffect : 0); 
-    }
-    public float GetDuration() { return duration; }
-
-    public void PerformAction(List<Goal> goals)
-    {
-        foreach (Goal g in goals)
-        {
-            // Check if the action has an effect on the given goal
-            if (g.goalName == effectingGoal)
-            {
-                 g.SatisfyGoal(this);
-            }
-        }
-    }
-}
-*/
 public class Action
 {
     public string actionName;
